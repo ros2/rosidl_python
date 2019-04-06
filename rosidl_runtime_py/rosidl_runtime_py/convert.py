e4fdb920cb6787a6b54cbb267d3c4aaf71e41a88
vinnamkim <vinnam.kim@gmail.com>
2019-04-06 16:28:41 +0900

    Add no_arr and no_str params  - For the issue : https://github.com/ros2/ros2cli/issues/136
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- rosidl_runtime_py/rosidl_runtime_py/convert.py
+++ rosidl_runtime_py/rosidl_runtime_py/convert.py
@@ -33,13 +33,16 @@ def __represent_ordereddict(dumper, data):
     return yaml.nodes.MappingNode(u'tag:yaml.org,2002:map', items)
 
 
-def message_to_yaml(msg: Any, truncate_length: int = None) -> str:
+def message_to_yaml(msg: Any, truncate_length: int = None,
+                    no_arr: bool = False, no_str: bool = False) -> str:
     """
     Convert a ROS message to a YAML string.
 
     :param msg: The ROS message to convert.
     :param truncate_length: Truncate values for all message fields to this length.
         This does not truncate the list of message fields.
+    :param no_arr: Exclude array fields of the message
+    :param no_str: Exclude string fields of the message
     :returns: A YAML string representation of the input ROS message.
     """
     global __yaml_representer_registered
@@ -50,24 +53,30 @@ def message_to_yaml(msg: Any, truncate_length: int = None) -> str:
         __yaml_representer_registered = True
 
     return yaml.dump(
-        message_to_ordereddict(msg, truncate_length=truncate_length),
-        allow_unicode=True, width=sys.maxsize,
+        message_to_ordereddict(
+            msg, truncate_length=truncate_length, no_arr=no_arr, no_str=no_str),
+        width=sys.maxsize, allow_unicode=True
     )
 
 
-def message_to_csv(msg: Any, truncate_length: int = None) -> str:
+def message_to_csv(msg: Any, truncate_length: int = None,
+                   no_arr: bool = False, no_str: bool = False) -> str:
     """
     Convert a ROS message to string of comma-separated values.
 
     :param msg: The ROS message to convert.
     :param truncate_length: Truncate values for all message fields to this length.
         This does not truncate the list of message fields.
+    :param no_arr: Exclude array fields of the message
+    :param no_str: Exclude string fields of the message
     :returns: A string of comma-separated values representing the input message.
     """
     def to_string(val):
         nonlocal truncate_length
         r = ''
         if any(isinstance(val, t) for t in [list, tuple, array.array, numpy.ndarray]):
+            if no_arr is True:
+                return r
             for i, v in enumerate(val):
                 if r:
                     r += ','
@@ -77,6 +86,8 @@ def message_to_csv(msg: Any, truncate_length: int = None) -> str:
                 r += to_string(v)
         elif any(isinstance(val, t) for t in [bool, bytes, float, int, str, numpy.number]):
             if any(isinstance(val, t) for t in [bytes, str]):
+                if no_str is True and isinstance(val, str):
+                    return r
                 if truncate_length is not None and len(val) > truncate_length:
                     val = val[:truncate_length]
                     if isinstance(val, bytes):
@@ -85,7 +96,7 @@ def message_to_csv(msg: Any, truncate_length: int = None) -> str:
                         val += '...'
             r = str(val)
         else:
-            r = message_to_csv(val, truncate_length)
+            r = message_to_csv(val, truncate_length, no_arr, no_str)
         return r
     result = ''
     # We rely on __slots__ retaining the order of the fields in the .msg file.
@@ -99,13 +110,19 @@ def message_to_csv(msg: Any, truncate_length: int = None) -> str:
 
 # Convert a msg to an OrderedDict. We do this instead of implementing a generic __dict__() method
 # in the msg because we want to preserve order of fields from the .msg file(s).
-def message_to_ordereddict(msg: Any, truncate_length: int = None) -> OrderedDict:
+def message_to_ordereddict(
+        msg: Any,
+        truncate_length: int = None,
+        no_arr: bool = False,
+        no_str: bool = False) -> OrderedDict:
     """
     Convert a ROS message to an OrderedDict.
 
     :param msg: The ROS message to convert.
     :param truncate_length: Truncate values for all message fields to this length.
         This does not truncate the list of fields (ie. the dictionary keys).
+    :param no_arr: Exclude array fields of the message
+    :param no_str: Exclude string fields of the message
     :returns: An OrderedDict where the keys are the ROS message fields and the values are
         set to the values of the input message.
     """
@@ -113,46 +130,53 @@ def message_to_ordereddict(msg: Any, truncate_length: int = None) -> OrderedDict
     # We rely on __slots__ retaining the order of the fields in the .msg file.
     for field_name in msg.__slots__:
         value = getattr(msg, field_name, None)
-        value = _convert_value(value, truncate_length=truncate_length)
+        value = _convert_value(
+            value, truncate_length=truncate_length, no_arr=no_arr, no_str=no_str)
         # Remove leading underscore from field name
         d[field_name[1:]] = value
     return d
 
 
-def _convert_value(value, truncate_length=None):
+def _convert_value(value, truncate_length=None, no_arr=False, no_str=False):
     if isinstance(value, bytes):
         if truncate_length is not None and len(value) > truncate_length:
             value = ''.join([chr(c) for c in value[:truncate_length]]) + '...'
         else:
             value = ''.join([chr(c) for c in value])
     elif isinstance(value, str):
-        if truncate_length is not None and len(value) > truncate_length:
+        if no_str is True:
+            value = ''
+        elif truncate_length is not None and len(value) > truncate_length:
             value = value[:truncate_length] + '...'
     elif isinstance(value, (list, tuple, array.array, numpy.ndarray)):
         # Since arrays and ndarrays can't contain mixed types convert to list
         typename = tuple if isinstance(value, tuple) else list
-        if truncate_length is not None and len(value) > truncate_length:
+        if no_arr is True:
+            value = ['']
+        elif truncate_length is not None and len(value) > truncate_length:
             # Truncate the sequence
             value = value[:truncate_length]
             # Truncate every item in the sequence
             value = typename(
-                [_convert_value(v, truncate_length) for v in value] + ['...'])
+                [_convert_value(v, truncate_length, no_arr, no_str) for v in value] + ['...'])
         else:
             # Truncate every item in the list
             value = typename(
-                [_convert_value(v, truncate_length) for v in value])
-    elif isinstance(value, (dict, OrderedDict)):
+                [_convert_value(v, truncate_length, no_arr, no_str) for v in value])
+    elif isinstance(value, (dict, OrderedDict)) or isinstance(value, OrderedDict):
         # Convert each key and value in the mapping
         new_value = {} if isinstance(value, dict) else OrderedDict()
         for k, v in value.items():
             # Don't truncate keys because that could result in key collisions and data loss
-            new_value[_convert_value(k)] = _convert_value(v, truncate_length=truncate_length)
+            new_value[_convert_value(k)] = _convert_value(
+                v, truncate_length=truncate_length, no_arr=no_arr, no_str=no_str)
         value = new_value
     elif isinstance(value, numpy.number):
         value = value.item()
     elif not isinstance(value, (bool, float, int)):
         # Assuming value is a message since it is neither a collection nor a primitive type
-        value = message_to_ordereddict(value, truncate_length=truncate_length)
+        value = message_to_ordereddict(
+            value, truncate_length=truncate_length, no_arr=no_arr, no_str=no_str)
     return value
 
 

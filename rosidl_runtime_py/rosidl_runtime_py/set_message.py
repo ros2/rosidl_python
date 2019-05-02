c31f096aba3ebf36ec4cba5c8c4990348d4f0475
Mikael Arguedas <mikael.arguedas@gmail.com>
2019-05-02 19:21:13 +0200

    support array and sequence of ROS messages
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- rosidl_runtime_py/rosidl_runtime_py/set_message.py
+++ rosidl_runtime_py/rosidl_runtime_py/set_message.py
@@ -15,6 +15,8 @@
 from typing import Any
 from typing import Dict
 
+from rosidl_runtime_py.import_message import import_message_type
+
 
 def set_message_fields(msg: Any, values: Dict[str, str]) -> None:
     """
@@ -33,4 +35,17 @@ def set_message_fields(msg: Any, values: Dict[str, str]) -> None:
         except TypeError:
             value = field_type()
             set_message_fields(value, field_value)
+        f_type = msg.get_fields_and_field_types()[field_name]
+        # Check if field is an array of ROS message types
+        if f_type.find('/') != -1:
+            if isinstance(field_type(), list):
+                # strip the 'sequence<' prefix if it is a sequence
+                if f_type.startswith('sequence'):
+                    f_type = f_type[len('sequence') + 1:]
+                f_type = f_type[:f_type.rfind('[')]
+                field_elem_type = import_message_type('', f_type)
+                for n in range(len(value)):
+                    submsg = field_elem_type()
+                    set_message_fields(submsg, value[n])
+                    value[n] = submsg
         setattr(msg, field_name, value)

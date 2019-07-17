2d66efcce3f961ed34ce02d009db16287635dc70
artivis <jeremie.deray@canonical.com>
2019-07-17 08:57:46 -0400

    NamespacedType object has no attribute value_type
      Fix #70.
    
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- rosidl_runtime_py/rosidl_runtime_py/import_message.py
+++ rosidl_runtime_py/rosidl_runtime_py/import_message.py
@@ -20,5 +20,5 @@ from rosidl_parser.definition import NamespacedType
 
 def import_message_from_namespaced_type(message_type: NamespacedType) -> Any:
     module = importlib.import_module(
-        '.'.join(message_type.value_type.namespaces))
-    return getattr(module, message_type.value_type.name)
+        '.'.join(message_type.namespaces))
+    return getattr(module, message_type.name)
